4105b1412d26a7f8675b4e49c2af85f4eadc0e91
Pariterre <pariterre@hotmail.com>
2026-03-27 15:02:57 -0400

    Stressed test inhouse data

--- runner/main_models_comparison.py
+++ runner/main_models_comparison.py
@@ -29,7 +29,7 @@ def _get_kinematics_metrics(
     subject: str,
     trial_type: TrialType,
     show_plot: bool = False,
-) -> dict[str, dict[str, AnalysesData]]:
+) -> dict[str, dict[str, AnalysesData | list[AnalysesData]]]:
     if trial_type == TrialType.SWAY:
         glb_filter = "quiet_stand"
         inhouse_filter = "quiet_stand"
@@ -41,9 +41,9 @@ def _get_kinematics_metrics(
 
     data_folder = kinematics_base_folder / subject
 
-    # Load the momentum_health data
-    glb_file_path = _load_single_file(data_folder, glb_filter, "glb")
-    momentum = GlbKinematicsData.from_file(glb_path=glb_file_path, trial_type=trial_type)
+    # # Load the momentum_health data
+    # glb_file_path = _load_single_file(data_folder, glb_filter, "glb")
+    # momentum = GlbKinematicsData.from_file(glb_path=glb_file_path, trial_type=trial_type)
 
     # Load the in-house data
     model_path = f"{model_base_folder}/{subject}/lower_body.bioMod"
@@ -53,15 +53,15 @@ def _get_kinematics_metrics(
         model_path=model_path, c3d_path=c3d_file_path, kinematics_path=inhouse_file_path, trial_type=trial_type
     )
 
-    # Align the data together
-    KinematicsData.perform_align_kinematics_data(inhouse, momentum, Side.LEFT, show_plot=show_plot)
+    # # Align the data together
+    # KinematicsData.perform_align_kinematics_data(inhouse, momentum, Side.LEFT, show_plot=show_plot)
 
     metrics = {}
     if trial_type == TrialType.SWAY:
         metrics["sway"] = {}
 
         metrics["sway"]["inhouse"] = SwayTrial.extract(kinematics_data=inhouse, show_plot=show_plot)
-        metrics["sway"]["momentum"] = SwayTrial.extract(kinematics_data=momentum, show_plot=show_plot)
+        # metrics["sway"]["momentum"] = SwayTrial.extract(kinematics_data=momentum, show_plot=show_plot)
 
     elif trial_type == TrialType.GAIT:
         metrics["left_cycles"] = {}
@@ -113,8 +113,7 @@ def main():
 
         # Show metrics
         if trial_type == TrialType.SWAY:
-            inhouse = metrics["sway"]["inhouse"]
-            sway = SwayTrial.extract(kinematics_data=inhouse, show_plot=False)
+            sway: SwayTrial = metrics["sway"]["inhouse"]
             print(f"Sway amplitude: {sway.amplitude(exclude_vertical=True).mean() * 1000:.2f} mm")
             print(f"Sway mean velocity: {sway.velocity(exclude_vertical=True).mean() * 1000:.2f} mm/s")
             print(f"Sway confidence ellipse: {sway.confidence_ellipse(confidence_level=0.95)}")
@@ -124,7 +123,7 @@ def main():
             if not left_cycles:
                 print("No left gait cycles found.")
             else:
-                first_cycle = left_cycles[0]
+                first_cycle: GaitCycle = left_cycles[0]
                 print(f"First left cycle gait speed: {first_cycle.mean_gait_speed(exclude_vertical=True)} m/s")
                 print(f"First left cycle stride length: {first_cycle.stride_length(exclude_vertical=True):.2f} m")
                 print(f"First left cycle stance time: {first_cycle.stance_time:.2f} s")
@@ -135,7 +134,7 @@ def main():
             if not right_cycles:
                 print("No right gait cycles found.")
             else:
-                first_cycle = right_cycles[0]
+                first_cycle: GaitCycle = right_cycles[0]
                 print(f"First right cycle gait speed: {first_cycle.mean_gait_speed(exclude_vertical=True)} m/s")
                 print(f"First right cycle stride length: {first_cycle.stride_length(exclude_vertical=True):.2f} m")
                 print(f"First right cycle stance time: {first_cycle.stance_time:.2f} s")
